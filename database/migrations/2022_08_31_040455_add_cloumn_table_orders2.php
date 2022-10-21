35c41601fd6a1bdf8279be82df47cf0d53365ebb
MinnKhanh <90829843+MinnKhanh@users.noreply.github.com>
2022-10-21 16:00:05 +0700

    procfiled

--- database/migrations/2022_08_31_040455_add_cloumn_table_orders2.php
+++ database/migrations/2022_08_31_040455_add_cloumn_table_orders2.php
@@ -14,7 +14,7 @@ class AddCloumnTableOrders2 extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->string('zip_code')->nullable();
+            $table->string('zip_code')->nullable()->change();
         });
     }
 
